9293d881df3cf526095a1802b3cf11d7d434c810
18Yqing <86784838+18Yqing@users.noreply.github.com>
2024-01-18 17:04:15 +0800

    test.sh

--- codes/test.sh
+++ codes/test.sh
@@ -1,1 +1,4 @@
-
+python eval.py  --decoder_folder DLR2534 --save_name test > ./result/test_DLR2534.txt
+python eval.py  --decoder_folder DLR3039 --save_name test > ./result/test_DLR3039.txt
+python eval.py  --decoder_folder DLR3542 --save_name test > ./result/test_DLR3542.txt
+python eval.py  --decoder_folder DLR4045 --save_name test > ./result/test_DLR4045.txt 
